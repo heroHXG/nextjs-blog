febce2395cf35e32844cf56ee9e84e3b269bd4f4
“heroHXG” <2068470260@qq.com>
2023-12-05 18:46:38 +0800

    add posts route

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,10 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
-import Date from '../components/date'
 
 import utilStyles from '../styles/utils.module.css'
 
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
-    
-    return {
-      props: {
-        allPostsData,
-        // postList
-      }
-    }
-}
-
-
-
-export default function Home({allPostsData, postList} ) {
-    // console.log(postList)
+export default function Home() {
   return (
     <Layout home>
       <Head>
@@ -48,25 +32,11 @@ export default function Home({allPostsData, postList} ) {
       <Link href={'/news/news'} prefetch={true}>
         <span style={{color: 'blue', fontWeight: 'bold', fontSize: '1.5rem'}}>daily-news</span>
       </Link>
-
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-                 id: {id} <br />
-                <Link legacyBehavior href={`/posts/${id}`}>
-                    <a style={{color: 'blue'}}>{title}</a>
-                </Link>
-               <br/>
-                <small className={utilStyles.lightText}>
-                    <Date dateString={date} />
-                </small>
-            </li>
-          ))}
-        </ul>
-       
-      </section>
+      <br/>
+      {/* /posts/index页面 */}
+      <Link href={'/posts'} prefetch={true}>
+        <span style={{color: 'blue', fontWeight: 'bold', fontSize: '1.5rem'}}>my-posts</span>
+      </Link>
     </Layout>
   )
 }
